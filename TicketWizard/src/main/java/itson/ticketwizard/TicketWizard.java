32315fdcd75f3c43859e90b9ac8a1c2359032e51
Pedro Luna <106363641+iPeterMoon@users.noreply.github.com>
2025-02-18 11:17:29 -0700

    Creación de la carpeta del proyecto

---
+++ TicketWizard/src/main/java/itson/ticketwizard/TicketWizard.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package itson.ticketwizard;
+
+/**
+ *
+ * @author Peter
+ */
+public class TicketWizard {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
