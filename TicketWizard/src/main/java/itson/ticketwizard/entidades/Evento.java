7e5292f1f2d4cc68b5632e95cb00f1760e909ad0
TRMrDucky <vv094673@gmail.com>
2025-02-19 07:49:52 -0700

    update Evento.java

--- TicketWizard/src/main/java/itson/ticketwizard/entidades/Evento.java
+++ TicketWizard/src/main/java/itson/ticketwizard/entidades/Evento.java
@@ -2,6 +2,11 @@ package itson.ticketwizard.entidades;
 
 public class Evento{
     
+    private String nombre;
+    private String ciudad;
+    private String estado;
+    private String descripcion;
+    private String recinto;
     
     
     
