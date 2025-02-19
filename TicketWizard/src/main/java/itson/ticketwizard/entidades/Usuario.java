4fe5433e4e0432fed9ac845ce635c3377c375f78
TRMrDucky <vv094673@gmail.com>
2025-02-19 08:58:44 -0700

    Agregada la clase Usuario / Agregados el DAO

---
+++ TicketWizard/src/main/java/itson/ticketwizard/entidades/Usuario.java
@@ -0,0 +1,11 @@
+package itson.ticketwizard.entidades;
+
+public class Usuario{
+    private String nombres;
+    private String apellidoMaterno;
+    private String apellidoPaterno;
+    private String correoElectronico;
+    private double saldo;
+    private Domicilio domicilio;
+    private String 
+}
