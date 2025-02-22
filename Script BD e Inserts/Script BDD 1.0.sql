4ff489a2b28e0b841f237751242f752130f1a388
TRMrDucky <vv094673@gmail.com>
2025-02-22 13:38:33 -0700

    Restauración DB #53256

--- Script BD e Inserts/Script BDD 1.0.sql
+++ Script BD e Inserts/Script BDD 1.0.sql
@@ -4,31 +4,31 @@ use ticketWizard;
 
 Drop Table If Exists Direcciones;
 Create Table Direcciones (
-idDireccion Int Primary Key AUTO_INCREMENT,
+	idDireccion Int Primary Key AUTO_INCREMENT,
     calle Varchar (100) Not Null,
     colonia Varchar (100) Not Null,
-ciudad Varchar (100) Not Null,
+	ciudad Varchar (100) Not Null,
     estado Varchar(100) Not Null,
     codigoPostal varchar(5) Not Null
 );
 
 Drop Table If Exists Usuarios;
 Create Table Usuarios (
-idUsuario Int Primary Key AUTO_INCREMENT,
-nombre Varchar (50) Not Null,
+	idUsuario Int Primary Key AUTO_INCREMENT,
+	nombre Varchar (50) Not Null,
     apellidoMaterno Varchar(50) Not Null,
     apellidoPaterno Varchar(50) Not Null,
     correoElectronico Varchar(150) Not Null,
-    contraseña Varchar(100) Not Null,
+    contraseña Varchar(100) Not Null,
     fechaNacimiento Date Not Null,
     saldo double(10,2) Not Null,
-    idDireccion int Not Null,
+    idDireccion int,
     Foreign Key (idDireccion) References Direcciones(idDireccion)
 );
   
 Drop Table If Exists Transacciones;
 Create Table Transacciones (
-idTransaccion Int Primary Key AUTO_INCREMENT,
+	idTransaccion Int Primary Key AUTO_INCREMENT,
     fechaHora DateTime Not Null,
     TipoCompra Enum ("Boletera", "Reventa") Not Null,
     monto double(10,2) Not Null
@@ -36,7 +36,7 @@ idTransaccion Int Primary Key AUTO_INCREMENT,
 
 Drop Table If Exists Usuarios_Transacciones;
 Create Table Usuarios_Transacciones (
-idUsuarioTransaccion Int Primary Key AUTO_INCREMENT,
+	idUsuarioTransaccion Int Primary Key AUTO_INCREMENT,
     rol Enum("Comprador", "Vendedor") Not Null,
     idUsuario Int Not Null,
     idTransaccion Int Not Null,
@@ -46,8 +46,8 @@ idUsuarioTransaccion Int Primary Key AUTO_INCREMENT,
 
 Drop Table If Exists Eventos;
 Create Table Eventos (
-idEvento Int Primary Key AUTO_INCREMENT,
-nombre Varchar (100) Not Null,
+	idEvento Int Primary Key AUTO_INCREMENT,
+	nombre Varchar (100) Not Null,
     fecha Date Not Null,
     ciudad Varchar (50) Not Null,
     Estado Varchar (50) Not Null,
@@ -57,27 +57,33 @@ nombre Varchar (100) Not Null,
 
 Drop Table If Exists Boletos;
 Create Table Boletos (
-idBoleto Int Primary Key AUTO_INCREMENT,
+	idBoleto VARCHAR(15) Primary Key,
     idEvento Int Not Null,
-    numSerie Varchar (8) Not Null,
+    numSerie Varchar (8) Not Null Unique,
     fila Varchar(5) Not Null,
     asiento Int Not Null,
     Disponible Bool Default 0,
-    precio decimal(10,2) Not Null,
-    precioOriginal decimal(10,2) Not Null,
-    idUsuario int null,
-    Foreign Key (idEvento) References Eventos(idEvento),
-    Foreign Key (idUsuario) References Usuarios(idUsuario)
+    precio decimal(10,2),
+    precioOriginal decimal(10,2),
+    idUsuario int,
+    foreign key (idUsuario) references Usuarios(idUsuario),
+    Foreign Key (idEvento) References Eventos(idEvento)
 );
 
 Drop Table If Exists TransaccionesBoletos;
 Create Table TransaccionesBoletos (
-idTransaccionBoletos Int Primary Key AUTO_INCREMENT,
+	idTransaccionBoletos Int Primary Key AUTO_INCREMENT,
     idTransaccion Int Not Null,
-    idBoleto Int Not Null,
+    idBoleto varchar(15) Not Null,
     Foreign Key (idTransaccion) References Transacciones(idTransaccion),
     Foreign Key (idBoleto) References Boletos(idBoleto)
 );
 
-
+Drop Table If Exists Reservas;
+Create table Reservas(
+	idUsuario Int not null,
+    disponibleHasta TimeStamp Default (Current_TimeStamp + Interval 10 Minute),
+    idBoleto varchar(15) primary key,
+    Foreign Key (idBoleto) References Boletos(idBoleto)
+);
 
--- Script BD e Inserts/insertsPoblacion.sql
+++ Script BD e Inserts/insertsPoblacion.sql
@@ -2,40 +2,40 @@ USE ticketWizard;
 
 -- Insertar direcciones
 INSERT INTO Direcciones (calle, colonia, ciudad, estado, codigoPostal) VALUES
-('Av. Insurgentes Sur', 'Roma Norte', 'Ciudad de México', 'CDMX', '06700'),
-('Blvd. Agua Caliente', 'Aviación', 'Tijuana', 'Baja California', '22000'),
-('Av. Revolución', 'Centro', 'Guadalajara', 'Jalisco', '44100'),
-('Calle 60', 'Centro', 'Mérida', 'Yucatán', '97000'),
-('Av. Paseo de la Reforma', 'Juárez', 'Ciudad de México', 'CDMX', '06600');
+('Av. Insurgentes Sur', 'Roma Norte', 'Ciudad de México', 'CDMX', '06700'),
+('Blvd. Agua Caliente', 'Aviación', 'Tijuana', 'Baja California', '22000'),
+('Av. Revolución', 'Centro', 'Guadalajara', 'Jalisco', '44100'),
+('Calle 60', 'Centro', 'Mérida', 'Yucatán', '97000'),
+('Av. Paseo de la Reforma', 'Juárez', 'Ciudad de México', 'CDMX', '06600');
 
 -- Insertar usuarios
-INSERT INTO Usuarios (nombre, apellidoMaterno, apellidoPaterno, correoElectronico, contraseña, fechaNacimiento, saldo, idDireccion) VALUES
-('Juan', 'Pérez', 'Gómez', 'juan.perez@gmail.com', 'password123', '1990-05-15', 1500.00, 1),
-('María', 'López', 'Hernández', 'maria.lopez@gmail.com', 'password456', '1985-08-22', 2000.00, 2),
-('Carlos', 'García', 'Martínez', 'carlos.garcia@gmail.com', 'password789', '1995-02-10', 1000.00, 3),
-('Ana', 'Martínez', 'Sánchez', 'ana.martinez@gmail.com', 'password012', '1988-11-30', 2500.00, 4),
-('Luis', 'Rodríguez', 'Díaz', 'luis.rodriguez@gmail.com', 'password345', '1992-07-25', 3000.00, 5);
+INSERT INTO Usuarios (nombre, apellidoMaterno, apellidoPaterno, correoElectronico, contraseña, fechaNacimiento, saldo, idDireccion) VALUES
+('Juan', 'Pérez', 'Gómez', 'juan.perez@gmail.com', 'password123', '1990-05-15', 1500.00, 1),
+('María', 'López', 'Hernández', 'maria.lopez@gmail.com', 'password456', '1985-08-22', 2000.00, 2),
+('Carlos', 'García', 'Martínez', 'carlos.garcia@gmail.com', 'password789', '1995-02-10', 1000.00, 3),
+('Ana', 'Martínez', 'Sánchez', 'ana.martinez@gmail.com', 'password012', '1988-11-30', 2500.00, 4),
+('Luis', 'Rodríguez', 'Díaz', 'luis.rodriguez@gmail.com', 'password345', '1992-07-25', 3000.00, 5);
 
 -- Insertar eventos
 INSERT INTO Eventos (nombre, fecha, ciudad, Estado, descripcion, recinto) VALUES
-('Concierto de Coldplay', '2023-12-15', 'Ciudad de México', 'CDMX', 'Concierto en el Foro Sol.', 'Foro Sol'),
-('Festival Cervantino', '2023-10-20', 'Guanajuato', 'Guanajuato', 'Festival cultural en Guanajuato.', 'Teatro Juárez'),
+('Concierto de Coldplay', '2023-12-15', 'Ciudad de México', 'CDMX', 'Concierto en el Foro Sol.', 'Foro Sol'),
+('Festival Cervantino', '2023-10-20', 'Guanajuato', 'Guanajuato', 'Festival cultural en Guanajuato.', 'Teatro Juárez'),
 ('Lucha Libre AAA', '2023-11-10', 'Guadalajara', 'Jalisco', 'Evento de lucha libre en la Arena VFG.', 'Arena VFG'),
-('Feria de León', '2024-01-12', 'León', 'Guanajuato', 'Feria tradicional en León.', 'Poliforum León'),
-('Concierto de Bad Bunny', '2023-09-30', 'Monterrey', 'Nuevo León', 'Concierto en el Estadio BBVA.', 'Estadio BBVA');
+('Feria de León', '2024-01-12', 'León', 'Guanajuato', 'Feria tradicional en León.', 'Poliforum León'),
+('Concierto de Bad Bunny', '2023-09-30', 'Monterrey', 'Nuevo León', 'Concierto en el Estadio BBVA.', 'Estadio BBVA');
 
 -- Insertar boletos
-INSERT INTO Boletos (idEvento, numSerie, fila, asiento, Disponible, precio) VALUES
-(1, 'A1B2C3D4', 'A', 1, 1, 500),
-(1, 'E5F6G7H8', 'B', 2, 1,7841),
-(2, 'I9J0K1L2', 'C', 3, 1,700),
-(2, 'M3N4O5P6', 'D', 4, 1,612),
-(3, 'Q7R8S9T0', 'E', 5, 1,1024),
-(3, 'U1V2W3X4', 'F', 6, 1,976),
-(4, 'Y5Z6A7B8', 'G', 7, 1,1358),
-(4, 'C9D0E1F2', 'H', 8, 1,35813),
-(5, 'G3H4I5J6', 'I', 9, 1,5813),
-(5, 'K7L8M9N0', 'J', 10, 1,8132);
+INSERT INTO Boletos (idBoleto, idEvento, numSerie, fila, asiento, Disponible, precio, precioOriginal) VALUES
+("JAKSJ",1, 'A1B2C3D4', 'A', 1, 1, 500, 500),
+("AKLSDK",1, 'E5F6G7H8', 'B', 2, 1,7841,500),
+("JAKJDSKSAKD",2, 'I9J0K1L2', 'C', 3, 1,700,500),
+("ASDASD",2, 'M3N4O5P6', 'D', 4, 1,612,500),
+("ASDASDAS",3, 'Q7R8S9T0', 'E', 5, 1,1024, 1024),
+("SDASD",3, 'U1V2W3X4', 'F', 6, 1,976, 976),
+("ASDKLSD",4, 'Y5Z6A7B8', 'G', 7, 1,1358, 1358),
+("KASLSKDL",4, 'C9D0E1F2', 'H', 8, 1,35813, 35813),
+("WKLFLDS",5, 'G3H4I5J6', 'I', 9, 1,500, 500),
+("LLSÑDAL",5, 'K7L8M9N0', 'J', 10, 1,500, 500);
 
 -- Insertar transacciones
 INSERT INTO Transacciones (fechaHora, TipoCompra, monto) VALUES
@@ -60,14 +60,13 @@ INSERT INTO Usuarios_Transacciones (rol, idUsuario, idTransaccion) VALUES
 
 -- Insertar transacciones_boletos
 INSERT INTO TransaccionesBoletos (idTransaccion, idBoleto) VALUES
-(1, 1),
-(1, 2),
-(2, 3),
-(2, 4),
-(3, 5),
-(3, 6),
-(4, 7),
-(4, 8),
-(5, 9),
-(5, 10);
-
+(1, "JAKSJ"),
+(1, "ASDASDAS"),
+(2, "ASDASDAS"),
+(2, "ASDASDAS"),
+(3, "ASDASDAS"),
+(3, "ASDASDAS"),
+(4, "ASDASDAS"),
+(4, "ASDASDAS"),
+(5, "ASDASDAS"),
+(5, "ASDASDAS");
