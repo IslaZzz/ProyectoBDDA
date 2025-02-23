c26db2383a9bb1100f2c0d2f0319101b659720f0
IslaZzz <99843612+IslaZzz@users.noreply.github.com>
2025-02-22 18:41:25 -0700

    correcion

--- TicketWizard/src/main/java/itson/ticketwizard/persistencia/UsuariosDAO.java
+++ TicketWizard/src/main/java/itson/ticketwizard/persistencia/UsuariosDAO.java
@@ -18,7 +18,6 @@ import java.sql.Date;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.util.Date;
 import java.util.LinkedList;
 import java.util.List;
 import javax.swing.JOptionPane;
