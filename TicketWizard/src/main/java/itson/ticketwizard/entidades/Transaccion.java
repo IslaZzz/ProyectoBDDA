337d4f069e8162ad0027e05ed52a44b75cc3366b
IslaZzz <99843612+IslaZzz@users.noreply.github.com>
2025-02-22 18:39:46 -0700

    Consultar transacciones
    Método agregado para usuarios y cambios al constructor de transacciones

--- TicketWizard/src/main/java/itson/ticketwizard/entidades/Transaccion.java
+++ TicketWizard/src/main/java/itson/ticketwizard/entidades/Transaccion.java
@@ -8,17 +8,28 @@ import java.util.Objects;
  * @author abrilislas
  */
 public class Transaccion {
+    private Integer idTransaccion;
     private Integer numeroControl;
     private Date fechaHora;
     private String tipoCompra;
     private double monto;
-    //considerar agregar foreign keys
+    private String rol;
+    private Integer idEvento;
+    private String idUsuario;
+    private int asiento;
+    private String fila;
+    private String evento;
 
-    public Transaccion(Integer numeroControl, Date fechaHora, String tipoCompra, double monto) throws TransaccionException{
-
+    public Transaccion(Integer idTransaccion,Integer numeroControl, Date fechaHora, String tipoCompra, double monto, String rol, String idUsuario, int asiento, String fila, String evento) throws TransaccionException{
+        this.idTransaccion=idTransaccion;
         this.numeroControl=numeroControl;
         this.fechaHora=fechaHora;
         this.monto=monto;
+        this.rol=rol;
+        this.idUsuario=idUsuario;
+        this.asiento=asiento;
+        this.fila=fila;
+        this.evento=evento;
 
         try{
             if(!tipoCompra.equals("Boletera") ||!tipoCompra.equals("Reventa")){
@@ -64,6 +75,64 @@ public class Transaccion {
         this.monto = monto;
     }
 
+    public String getRol() {
+        return rol;
+    }
+
+    public void setRol(String rol) {
+        this.rol = rol;
+    }
+
+    public Integer getIdEvento() {
+        return idEvento;
+    }
+
+    public void setIdEvento(Integer idEvento) {
+        this.idEvento = idEvento;
+    }
+
+    public String getIdUsuario() {
+        return idUsuario;
+    }
+
+    public void setIdUsuario(String idUsuario) {
+        this.idUsuario = idUsuario;
+    }
+
+    public int getAsiento() {
+        return asiento;
+    }
+
+    public void setAsiento(int asiento) {
+        this.asiento = asiento;
+    }
+
+    public String getFila() {
+        return fila;
+    }
+
+    public void setFila(String fila) {
+        this.fila = fila;
+    }
+
+    public String getEvento() {
+        return evento;
+    }
+
+    public void setEvento(String evento) {
+        this.evento = evento;
+    }
+
+    public Integer getIdTransaccion() {
+        return idTransaccion;
+    }
+
+    public void setIdTransaccion(Integer idTransaccion) {
+        this.idTransaccion = idTransaccion;
+    }
+    
+    
+
     @Override
     public int hashCode() {
         int hash = 5;
@@ -90,8 +159,10 @@ public class Transaccion {
 
     @Override
     public String toString() {
-        return "Transaccion: " + "numeroControl: " + numeroControl + ", fechaHora: " + fechaHora + ", tipoCompra: " + tipoCompra + ", monto: " + monto;
+        return "Transaccion{" + "numeroControl=" + numeroControl + ", fechaHora=" + fechaHora + ", tipoCompra=" + tipoCompra + ", monto=" + monto + ", rol=" + rol + ", idEvento=" + idEvento + ", idUsuario=" + idUsuario + ", asiento=" + asiento + ", fila=" + fila + ", evento=" + evento + '}';
     }
+
+    
 
 
 
--- TicketWizard/src/main/java/itson/ticketwizard/persistencia/UsuariosDAO.java
+++ TicketWizard/src/main/java/itson/ticketwizard/persistencia/UsuariosDAO.java
@@ -8,13 +8,17 @@ import itson.ticketwizard.dto.NuevaDireccionDTO;
 import itson.ticketwizard.dto.RegistroUsuarioDTO;
 import itson.ticketwizard.entidades.Boleto;
 import itson.ticketwizard.entidades.Seguridad;
+import itson.ticketwizard.entidades.Transaccion;
 import itson.ticketwizard.entidades.Usuario;
+import itson.ticketwizard.excepciones.TransaccionException;
 import itson.ticketwizard.persistencia.interfaces.IUsuariosDAO;
 import java.sql.CallableStatement;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.Date;
+import java.util.LinkedList;
 import java.util.List;
 import javax.swing.JOptionPane;
 
@@ -143,5 +147,41 @@ public class UsuariosDAO implements IUsuariosDAO {
             return usuario.getSaldo();
         }
         
+        public List<Transaccion> consultarMovimientos(Usuario usuario) throws TransaccionException{
+            int id = usuario.getId();
+            List<Transaccion> listaTransacciones = new LinkedList<>();
+            String consultarMovimientos=  """
+                                          CALL consultarTransacciones(?);
+                                          """;
+            try{
+                Connection conexion = manejadorConexion.crearConexion();
+                PreparedStatement consulta = conexion.prepareStatement(consultarMovimientos);
+                consulta.setInt(1, id);
+                ResultSet resultadoConsulta = consulta.executeQuery();
+                int filasActualizadas = consulta.executeUpdate();
+                
+                while(resultadoConsulta.next()){
+                    
+                    Integer idTransaccion = resultadoConsulta.getInt("ID de la transacción");
+                    Integer numeroControl = resultadoConsulta.getInt("Número de control");
+                    Date fechaHora = resultadoConsulta.getDate("Fecha-hora");
+                    String tipoCompra = resultadoConsulta.getString("Tipo de compra");
+                    double monto = resultadoConsulta.getDouble("Monto");
+                    String rol = resultadoConsulta.getString("Rol");
+                    String idUsuario = resultadoConsulta.getString("ID usuario");
+                    String fila = resultadoConsulta.getString("Fila");
+                    int asiento = resultadoConsulta.getInt("Asiento");
+                    String evento = resultadoConsulta.getString("Evento");
+                    
+                    Transaccion tran = new Transaccion(idTransaccion, numeroControl, fechaHora, tipoCompra, monto, rol, idUsuario, asiento,fila,evento);
+                    listaTransacciones.add(tran);
+                }
+            
+            }catch(SQLException ex){
+                ex.getMessage();
+                }
+            
+        return listaTransacciones;
+        }
         
 }
